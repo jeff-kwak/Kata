62ec2a38fc95410dee949387fc7d3224ca5a8a07
jeff <jeff.kwak@gmail.com>
2025-01-27 07:48:48 -0500

    feat: create user

--- src/Kata.Web/Toolkit/Functional/EitherLinqExtensions.cs
+++ src/Kata.Web/Toolkit/Functional/EitherLinqExtensions.cs
@@ -2,8 +2,17 @@ namespace Kata.Web.Toolkit.Functional;
 
 public static class EitherLinqExtensions
 {
-    public static Either<R,P> Select<T,R,P>(this Either<T,P> either, Func<T,R> mapper) => either.Map(mapper);
-    public static Either<T,P> Where<T,P>(this Either<T,P> either, Func<T,bool> predicate) => either.Bind(ok => predicate(ok) ? Either<T,P>.Ok(ok) : Either<T,P>.Problem(default!));
-    public static Either<R,P> SelectMany<T, P, U, R>(this Either<T,P> either, Func<T, Either<U,P>> bind, Func<T, U, R> map) =>
-        either.Bind(obj => bind(obj).Map(result => map(obj, result)));
+    public static Either<R,P> Select<T,R,P>(this Either<T,P> either, Func<T,R> mapper) =>
+        either.Map(mapper);
+
+
+    public static Either<T,P> Where<T,P>(this Either<T,P> either, Func<T,bool> predicate) =>
+        either.Bind(ok => predicate(ok) ? Either<T,P>.Ok(ok) : Either<T,P>.Problem(default!));
+
+
+    public static Either<R,P> SelectMany<T, P, U, R>(
+        this Either<T,P> either,
+        Func<T, Either<U,P>> bind,
+        Func<T, U, R> map) =>
+            either.Bind(obj => bind(obj).Map(result => map(obj, result)));
 }
